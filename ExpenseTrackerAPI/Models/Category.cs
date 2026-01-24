131772969b56c2c311919e4147841ef7c3ae45d5
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2026-01-24 20:28:27 +0500

    Creating Models for the project.

---
+++ ExpenseTrackerAPI/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTrackerAPI.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string? Name { get; set; } = string.Empty;
+        [MaxLength(7)]
+        public string? ColorCode { get; set; } = "#000000";
+
+        public ICollection<Expense>? Expenses { get; set; } = new List<Expense>();
+    }
+}
---
+++ ExpenseTrackerAPI/Models/Expense.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTrackerAPI.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+
+        [Required]
+        public int CategoryId { get; set; }
+        public DateTime Date { get; set; }
+        public string? Description { get; set; }
+        public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
