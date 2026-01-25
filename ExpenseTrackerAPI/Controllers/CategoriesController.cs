4c420c12833a43674124f8611dd244a6292c4ac9
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2026-01-25 22:14:40 +0500

    Making a get request for the categories model

--- ExpenseTrackerAPI/Controllers/CategoriesController.cs
+++ ExpenseTrackerAPI/Controllers/CategoriesController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ExpenseTrackerAPI.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExpenseTrackerAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CategoriesController : ControllerBase
+    public class CategoriesController(ExpenseDbContext context) : ControllerBase
     {
+        private readonly ExpenseDbContext _context = context;
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategories()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return Ok(categories);
+        }
     }
 }
