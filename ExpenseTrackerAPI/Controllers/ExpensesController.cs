8df20161bb647bc536a03b0829453df11d0029a4
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2026-01-26 20:47:55 +0500

    Working with the Expense Controller

---
+++ ExpenseTrackerAPI/Controllers/ExpensesController.cs
@@ -0,0 +1,42 @@
+﻿using ExpenseTrackerAPI.Data;
+using ExpenseTrackerAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTrackerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ExpensesController(ExpenseDbContext context) : ControllerBase
+    {
+        private readonly ExpenseDbContext _context = context;
+
+        [HttpGet]
+        public async Task<IActionResult> GetExpense()
+        {
+            var expenses = await _context.Expenses.OrderBy(x => x.CreatedAt).Include(x=> x.Category).ToListAsync();
+            return Ok(expenses);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetExpenseById([FromBody] int id)
+        {
+            var expense = await _context.Expenses.FindAsync(id);
+            if ( expense is null)
+            {
+                return NotFound();
+            }
+            return Ok(expense);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult<Expense>> CreateExpense([FromBody] Expense expense)
+        
+        { 
+            
+        }
+
+    }
+}
