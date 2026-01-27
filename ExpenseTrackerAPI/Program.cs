70e0025d0677afc553df281319621728832dea8d
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2026-01-27 19:31:45 +0500

    Update Program.cs

--- ExpenseTrackerAPI/Program.cs
+++ ExpenseTrackerAPI/Program.cs
@@ -9,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ExpenseDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -26,6 +28,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Expense Tracker API v1");
+        c.RoutePrefix = "swagger";  // Access at /swagger
+    });
+
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
